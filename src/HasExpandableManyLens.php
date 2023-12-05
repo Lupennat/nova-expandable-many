0885113b4af171346d5243c8e6f0016a820f36a9
Claudio Pennati <claudio.pennati@gmail.com>
2023-12-05 20:08:02 +0100

    lens fix

---
+++ src/HasExpandableManyLens.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lupennat\ExpandableMany;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+trait HasExpandableManyLens
+{
+    /**
+     * Resolve the given fields to their values.
+     *
+     * @return \Laravel\Nova\Fields\FieldCollection<int, \Laravel\Nova\Fields\Field>
+     */
+    public function resolveFields(NovaRequest $request)
+    {
+        return $this->availableFields($request)
+            ->filterForIndex($request, $this->resource)
+            ->withoutListableFieldsNotExpandable()
+            ->authorized($request)
+            ->resolveForDisplay($this->resource);
+    }
+}
