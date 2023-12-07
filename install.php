44679cf21d4254bc78265a5726fac912de9016cb
Claudio Pennati <claudio.pennati@gmail.com>
2023-12-07 12:33:41 +0100

    improve build

--- install.php
+++ install.php
@@ -5,7 +5,7 @@ const MINIMUMVERSION = '4.32.7';
 copy(__DIR__ . '/composer.json', __DIR__ . '/composer.json.original');
 
 $content = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
-$content['require']['laravel/nova'] = '4.32.7';
+$content['require']['laravel/nova'] = MINIMUMVERSION;
 
 file_put_contents(__DIR__ . '/composer.json', json_encode($content, JSON_PRETTY_PRINT));
 
