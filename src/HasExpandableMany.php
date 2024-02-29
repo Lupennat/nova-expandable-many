b064cbc5d3f5a6e2eedbf9cd66244aea3520e913
mathieufrh <mathieu.frh@live.de>
2024-02-29 11:07:07 +0100

    Re-order includes

--- src/HasExpandableMany.php
+++ src/HasExpandableMany.php
@@ -2,8 +2,8 @@
 
 namespace Lupennat\ExpandableMany;
 
-use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Fields\FieldCollection;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 trait HasExpandableMany
 {
